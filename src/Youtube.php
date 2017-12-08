f67140a2dd879ba2209e4aeaf124c4f9b09158f0
Aalaap Ghag <aalaap@gmail.com>
2017-12-08 12:10:16 +0530

    Remove method name collisions
    Added a 'youtube' prefix to all methods to avoid collisions with other Faker methods.

--- src/Youtube.php
+++ src/Youtube.php
@@ -1,8 +1,10 @@
-<?php namespace Faker\Provider;
+<?php
+
+namespace Faker\Provider;
 
 class Youtube extends Base
 {
-    public function id()
+    public function youtubeId()
     {
         $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
             . 'abcdefghijklmnopqrstuvwxyz_-';
@@ -12,24 +14,24 @@ class Youtube extends Base
         return $this->generator->parse($id);
     }
 
-    public function uri()
+    public function youtubeUri()
     {
-        return 'www.youtube.com/watch?v=' . $this->id();
+        return 'www.youtube.com/watch?v=' . $this->youtubeId();
     }
 
-    public function shortUri()
+    public function youtubeShortUri()
     {
-        return 'youtu.be/' . $this->id();
+        return 'youtu.be/' . $this->youtubeId();
     }
 
-    public function embedUri()
+    public function youtubeEmbedUri()
     {
-        return 'www.youtube.com/embed/' . $this->id();
+        return 'www.youtube.com/embed/' . $this->youtubeId();
     }
 
-    public function embedCode()
+    public function youtubeEmbedCode()
     {
-        return '<iframe width="560" height="315" src="' . $this->embedUri()
+        return '<iframe width="560" height="315" src="' . $this->youtubeEmbedUri()
             . '" frameborder="0" gesture="media" allow="encrypted-media"'
             . ' allowfullscreen></iframe>';
     }
@@ -38,17 +40,17 @@ class Youtube extends Base
     {
         switch (mt_rand(1,3)) {
             case 1:
-                return $this->uri();
+                return $this->youtubeUri();
 
                 break;
 
             case 1:
-                return $this->shortUri();
+                return $this->youtubeShortUri();
 
                 break;
 
             case 1:
-                return $this->embedUri();
+                return $this->youtubeEmbedUri();
 
                 break;
         }
