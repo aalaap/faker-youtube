eff2f96ee3f9a845d23140dd1721b14103b5c4d3
Aalaap Ghag <aalaap@gmail.com>
2020-11-07 12:13:34 +0530

    Fix tests

--- tests/Faker/YoutubeTest.php
+++ tests/Faker/YoutubeTest.php
@@ -15,34 +15,34 @@ class YoutubeTest extends TestCase
      */
     private $faker;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $faker = Factory::create();
         $faker->addProvider(new Youtube($faker));
         $this->faker = $faker;
     }
 
-    public function testYoutubeUri()
+    public function testYoutubeUri(): void
     {
         $this->assertRegExp('#(http(s)??\:\/\/)?(www\.)?(youtube\.com\/watch\?v=)(.+)#', $this->faker->youtubeUri());
     }
 
-    public function testYoutubeShortUri()
+    public function testYoutubeShortUri(): void
     {
         $this->assertRegExp('#(http(s)??\:\/\/)?(www\.)?(youtu.be\/)(.+)#', $this->faker->youtubeShortUri());
     }
 
-    public function testYoutubeEmbedUri()
+    public function testYoutubeEmbedUri(): void
     {
         $this->assertRegExp('#(http(s)??\:\/\/)?(www\.)?(youtube\.com\/embed\/)(.+)#', $this->faker->youtubeEmbedUri());
     }
 
-    public function testYoutubeChannelUri()
+    public function testYoutubeChannelUri(): void
     {
         $this->assertRegExp('#(http(s)??\:\/\/)?(www\.)?(youtube\.com\/)(c\/|channel\/|user\/)([a-zA-Z0-9\-]{1,})#', $this->faker->youtubeChannelUri());
     }
 
-    public function testYoutubeEmbedCode()
+    public function testYoutubeEmbedCode(): void
     {
         $this->assertRegExp('#<iframe width="(\d+)" height="(\d+)" src="((http(s)??\:\/\/)?(www\.)?(youtube\.com\/embed\/)(.+))" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen><\/iframe>#', $this->faker->youtubeEmbedCode());
     }
